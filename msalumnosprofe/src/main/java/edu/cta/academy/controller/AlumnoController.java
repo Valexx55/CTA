347898ca1d7b096f8e1bc83634b25a2fb1596ba7
Valexx55 <valexx55@gmail.com>
2023-09-21 17:09:05 +0200

    docu

--- msalumnosprofe/src/main/java/edu/cta/academy/controller/AlumnoController.java
+++ msalumnosprofe/src/main/java/edu/cta/academy/controller/AlumnoController.java
@@ -1,7 +1,12 @@
 package edu.cta.academy.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.cta.academy.repository.entity.Alumno;
 
 /**
  * ESTA CLASE, RECIBE LAS PETICIONES DEL CLIENTE
@@ -9,6 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/alumno") //servidor, todo lo que sea /alumno, es para esta clase
 public class AlumnoController {
+	
+	//ALTA
+	//BAJA
+	//CONSULTAR UNO
+	//CONSULTAR TODOS
+	@GetMapping //GET http://localhost:8081/alumno
+	public ResponseEntity<?> listarAlumnos() // ResponseEntity representa el mensaje HTTP de respuesta
+	{
+		ResponseEntity<?> responseEntity = null;
+		Iterable<Alumno> ita = null;
+		
+		return responseEntity;
+	}
+	//MODIFICAR
 
 }
