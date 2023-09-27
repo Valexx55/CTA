a85b84b170ba8df59fa41e94c4b1816ef85c6235
Valexx55 <valexx55@gmail.com>
2023-09-27 20:52:10 +0200

    Eureka

--- msalumnosprofe/src/main/java/edu/cta/academy/MsalumnosprofeApplication.java
+++ msalumnosprofe/src/main/java/edu/cta/academy/MsalumnosprofeApplication.java
@@ -2,8 +2,10 @@ package edu.cta.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsalumnosprofeApplication {
 
 	public static void main(String[] args) {
